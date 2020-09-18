3832c881c624135ac8bd9421840bf844dbfea8f5
Jacob Bashista <jacob@orange.haus>
2020-09-18 11:38:33 -0400

    Added Webserver

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { Client } from 'discord.js';
+import express from 'express';
 import config from './config.json';
 import ChatFilter from './modules/ChatFilterModule';
 import FLLCommands from './modules/FLLCommandsModule';
@@ -23,10 +24,24 @@ class Rosie {
         this.bot.on('ready', () => {
             console.log(`Logged in as ${this.bot.user.tag}!`);
 
-
             this.bot.user.setActivity('.missions', {
                 type: 'LISTENING'
             });
+
+            const app = express()
+
+            app.get('/info', (req, res) => {
+                res.json({
+                    online: true,
+                    guilds: this.bot.guilds.cache.size,
+                    members: this.bot.users.cache.size
+                });
+            });
+
+            app.listen(1746, () => {
+                console.log('Web Server Running');
+            });
+
         });
     }
 }
